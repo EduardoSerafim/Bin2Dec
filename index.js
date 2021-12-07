a18f411bbf7d7ac98b2121ce55677b83cabf2ae4
Eduardo Serafim <eduserafim7@gmail.com>
2021-12-07 17:17:44 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function bin2dec(bin) {
+    var decimal = 0;
+    for (var i = bin.length - 1; i >= 0; i--) {
+        decimal += parseInt(bin[i]) * Math.pow(2, bin.length - 1 - i);
+    }
+    return decimal;
+}
+
+binInput = document.querySelector('.binInput').value
+conversao = document.querySelector('.conversao')
+botao = document.querySelector('.converter')
+
+botao.addEventListener('click', (event) => {
+        event.preventDefault()
+        binInput = document.querySelector('.binInput').value
+        let validNum = true
+        for (let index = 0; index < binInput.length; index++) {
+            if (binInput.charAt(index) != '1' && binInput.charAt(index) != '0') {
+                validNum = false
+                break
+            }
+        }
+        if (validNum == false) {    
+            conversao.innerHTML = ''
+            alert('apenas 0 ou 1 é válido')
+        } else {
+            base = '10'
+            conversao.innerHTML = `(${binInput}) ${'2'.sub()} = (${bin2dec(binInput)}) ${'10'.sub()}` // output --- (bin)2 = (dec)10  --- 2 e 10 vão estar subscritos
+        }
+
+    }
+
+)
